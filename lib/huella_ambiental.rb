7da26b7e4982c8abb45a1e52bde2d42f6b28754f
DanielGlezExp <daniel.280596@gmail.com>
2019-11-11 20:19:42 +0000

    Creada la práctica!

---
+++ lib/huella_ambiental.rb
@@ -0,0 +1,6 @@
+require "huella_ambiental/version"
+
+module HuellaAmbiental
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/huella_ambiental_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe HuellaAmbiental do
+  context "Existencia de la clase alimento y sus metodos / atributos" do
+    it "has a version number" do
+      expect(HuellaAmbiental::VERSION).not_to be nil
+    end
+
+    it "does something useful" do
+      expect(false).to eq(true)
+    end
+  end
+end
