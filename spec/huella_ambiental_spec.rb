3f25d3025e4f206cb53a389faeead9cfbd1cd1f6
DanielGlezExp <daniel.280596@gmail.com>
2019-11-11 21:09:59 +0000

    Creada la primera prueba!

--- spec/huella_ambiental_spec.rb
+++ spec/huella_ambiental_spec.rb
@@ -1,11 +1,20 @@
+require './lib/huella_ambiental'
+
 RSpec.describe HuellaAmbiental do
+  before (:all) do
+    @carne_vaca = {nombre: "carne_vaca", proteinas: 21.1, carbohidratos: 0.0, lipidos: 3.1, C02: 50.0, terreno: 164.0}
+    @nuez = {nombre: "nuez", proteinas: 20.0, carbohidratos: 21.0, lipidos: 54.0, C02: 0.3, terreno: 7.9}
+
+    @comidas = [@carne_vaca, @nuez]
+  end
+
   context "Existencia de la clase alimento y sus metodos / atributos" do
     it "has a version number" do
       expect(HuellaAmbiental::VERSION).not_to be nil
     end
 
-    it "does something useful" do
-      expect(false).to eq(true)
+    it "La clase esta definida" do
+      expect(defined?(Alimento)).to be true
     end
   end
 end
