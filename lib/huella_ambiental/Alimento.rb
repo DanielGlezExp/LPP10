ff84b05ad24ffae2dfa7376c9ac354bb9aa55aed
DanielGlezExp <daniel.280596@gmail.com>
2019-11-12 18:34:25 +0000

    Prueba superada! Puedo crear Alimentos y de diferentes manteras

--- lib/huella_ambiental/Alimento.rb
+++ lib/huella_ambiental/Alimento.rb
@@ -1,3 +1,21 @@
 class Alimento
+  attr_reader :nombre, :proteinas, :carbohidratos, :lipidos, :co2, :terreno, :cantidad
+  
+  def initialize (*args)
+    if args.length == 1 and args[0].instance_of?(Hash) then
+      hash_alimento = args[0]
+      inicializar(hash_alimento[:nombre], hash_alimento[:proteinas], hash_alimento[:carbohidratos], hash_alimento[:lipido], hash_alimento[:co2], hash_alimento[:terreno], hash_alimento[:cantidad] )
+    elsif args.length == 7 then
+      inicializar(args[0], args[1], args[2], args[3], args[4], args[5], args[6])
+    end
+  
+  end
+  
+  #MÉTODOS PRIVADOS
+  private
+
+  def inicializar (nombre, proteinas, carbohidratos, lipidos, co2, terreno, cantidad)
+    @nombre, @proteinas, @carbohidrados, @lipidos, @co2, @terreno, @cantidad = nombre, proteinas, carbohidratos, lipidos, co2, terreno, cantidad
+  end
 
 end
