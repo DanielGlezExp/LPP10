d3df8ed6769128fc21f888b2cbe4aa726932a650
DanielGlezExp <daniel.280596@gmail.com>
2019-11-12 19:39:02 +0000

    Añadida las pruebas para los metdos getters de alimento

--- lib/huella_ambiental/Alimento.rb
+++ lib/huella_ambiental/Alimento.rb
@@ -3,7 +3,7 @@ class Alimento
   def initialize (*args)
     if args.length == 1 and args[0].instance_of?(Hash) then
       hash_alimento = args[0]
-      inicializar(hash_alimento[:nombre], hash_alimento[:proteinas], hash_alimento[:carbohidratos], hash_alimento[:lipido], hash_alimento[:co2], hash_alimento[:terreno], hash_alimento[:cantidad] )
+      inicializar(hash_alimento[:nombre], hash_alimento[:proteinas], hash_alimento[:carbohidratos], hash_alimento[:lipidos], hash_alimento[:co2], hash_alimento[:terreno], hash_alimento[:cantidad] )
     elsif args.length == 7 then
       inicializar(args[0], args[1], args[2], args[3], args[4], args[5], args[6])
     end
--- spec/huella_ambiental_spec.rb
+++ spec/huella_ambiental_spec.rb
@@ -14,41 +14,53 @@ RSpec.describe HuellaAmbiental do
     end   
   end
   
+
   it "has a version number" do
     expect(HuellaAmbiental::VERSION).not_to be nil
   end
   
+
   context "Existencia de la clase Alimento y sus atributos" do
     it "La clase alimento esta definida" do
       expect(Object.const_defined?('Alimento')).to be true
     end
+
     it "La clase Alimento tiene un nombre para alimento" do
       expect(@hash_alimentos[:nuez].respond_to?(:nombre)).to be true 
     end
+
     it "La clase Alimento tiene una cantidad de proteinas" do
       expect(@hash_alimentos[:nuez].respond_to?(:proteinas)).to be true
     end
+
     it "La clase Alimento tiene una cantidad de carbohidrados" do
       expect(@hash_alimentos[:nuez].respond_to?(:carbohidratos)).to be true
     end
+
     it "La clase Alimento tiene una cantidad de lipidos" do
       expect(@hash_alimentos[:nuez].respond_to?(:lipidos)).to be true
     end
+
     it "La clase Alimento tiene una cantidad de gases de infecto invernadero asociada" do
       expect(@hash_alimentos[:nuez].respond_to?(:co2)).to be true
     end
+
     it "La clase Alimento tiene una cantidad de terreno utilizado asociada" do
       expect(@hash_alimentos[:carne_vaca].respond_to?(:terreno)).to be true
     end
+
     it "La clase alimento tiene una cantidad de alimento en kilogramos asociada" do
       expect(@hash_alimentos[:carne_vaca].respond_to?(:cantidad)).to be true
     end
+
   end
+
 
   context "Crear alimentos" do
     it "Puedo crear un alimento" do
       expect(Alimento.new("nuez", 20.0, 21.0, 54.0, 0.3, 7.9, 1.0).instance_of?(Alimento)).to be true
     end
+
     it "Puedo crear un alimento a partir de un hash" do
       expect(Alimento.new(@hash_nuez).instance_of?(Alimento)).to be true 
     end
@@ -60,6 +72,38 @@ RSpec.describe HuellaAmbiental do
       end
     end
 
+  end
+
+
+  context "Metodos de acceso a los datos de alimento" do
+    it "Puedo acceder el anombre" do
+      expect("nuez").to eq(@hash_alimentos[:nuez].nombre)
+    end
+
+    it "Puedo acceder a la cantidad de proteinas" do
+      expect(20.0).to eq(@hash_alimentos[:nuez].proteinas)
+    end
+
+    it "Puedo acceder a la cantidad de carbohidratos" do
+      expect(21.0).to eq(@hash_alimentos[:nuez].carbohidratos)
+    end
+
+    it "Puedo acceder a la cantidad de Lipidos" do
+      expect(54.0).to eq(@hash_alimentos[:nuez].lipidos)
+    end
+
+    it "Puedo acceder a la cantidad de co2" do
+      expect(0.3).to eq(@hash_alimentos[:nuez].co2)
+    end
+
+    it "Puedo acceder a la cantidad de terreno" do
+      expect(7.9).to eq(@hash_alimentos[:nuez].terreno)
+    end
+
+    it "Puedo acceder a la cantidad de comida en kilogramos" do
+      expect(1.0).to eq(@hash_alimentos[:nuez].cantidad)
+    end
+  
   end
 
 end
