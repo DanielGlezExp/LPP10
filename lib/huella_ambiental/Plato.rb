f2da306d41fa3978eb7dd0cbd70dc66a6b781c49
DanielGlezExp <daniel.280596@gmail.com>
2019-11-26 18:52:36 +0000

    superada

--- lib/huella_ambiental/Plato.rb
+++ lib/huella_ambiental/Plato.rb
@@ -1,5 +1,10 @@
 
 
 class Plato
+  attr_reader :lista_alimentos
 
+  def initialize(lista_alimentos)
+    raise TypeError, "El parametro de entrada deben ser instancias de la clase lista" unless lista_alimentos.instance_of?(Lista)
+    @lista_alimentos = lista_alimentos
+  end
 end
