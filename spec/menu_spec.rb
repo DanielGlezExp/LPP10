b570a802b647bcefd1e1c12fdf15cbb783d1b860
DanielGlezExp <daniel.280596@gmail.com>
2020-01-07 17:08:56 +0000

    Añadida prueba para el formateo de la clase menu

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -31,4 +31,16 @@ RSpec.describe Menu do
 			end
 		end
 	end
+
+	context "formateo" do
+		it "puedo mostrar el menu formateado" do
+			mi_menu = Menu.new("huevo y manzana") do
+				componente :plato => @plato1,
+					   :precio => 2.03
+				componente :plato => @plato2,
+					   :precio => 0.8
+			end
+			exepect(mi_menu).to eq("\tMenu: huevo y manzana\nPlatos:\nNombre: huevo frito | C02: 32 | Terreno: 20 | Kcal: 73\nNombre: manzana | C02: 3 | Terreno: 4 | Kcal: 20")
+		end
+	end
 end
